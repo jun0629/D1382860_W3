a164b3f278d6c69056694af290e7f8e12af233ad
jun0629 <130278879+jun0629@users.noreply.github.com>
2024-09-26 10:45:39 +0800

    first commit

---
+++ a0926.py
@@ -0,0 +1,8 @@
+a = int(input("輸入變數 a："))
+b = int(input("輸入變數 b："))
+
+a, b = b, a
+
+print("交換後的結果：")
+print(f"a = {a}")
+print(f"b = {b}")
---
+++ a09262.py
@@ -0,0 +1,6 @@
+a = int(input("輸入矩形的⻑："))
+b = int(input("輸入矩形的寬："))
+
+print("交換後的結果：")
+print("面積："f"a = {a*b}")
+print("周長："f"b = {a*2+b*2}")
